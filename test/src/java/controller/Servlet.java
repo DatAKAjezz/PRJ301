19fd3da87fcf8a2ed36ed4579e21cd071e714ecf
Dat Hoang Tan <jezzthedead@gmail.com>
2025-02-07 11:17:23 +0700

    u
    .

--- test/src/java/controller/Servlet.java
+++ test/src/java/controller/Servlet.java
@@ -7,8 +7,6 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javafx.scene.control.Alert;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,50 +17,42 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author datdat
  */
-@WebServlet(name = "MainController", urlPatterns = {"/MainController"})
-public class MainController extends HttpServlet {
+@WebServlet(name = "Servlet", urlPatterns = {"/Servlet"})
+public class Servlet extends HttpServlet {
 
-
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-         response.setContentType("text/html;charset=UTF-8");
-       
-        PrintWriter out = response.getWriter();
-         
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        
-        if (username.trim().length() == 0){
-            out.print("Username cant be left empty!");
-            return;
-        }
-        
-        if (password.trim().length() == 0){
-            out.print("Password cant be left empty!");
-            return;
-        }
-        
-                
-        try{
-          if (password.length() < 8) {
-              throw new Exception("Mat khau phai co it nhat 8 ky tu");
-          }
-          if (username.equals("admin") && password.equals("12345678")){
-              RequestDispatcher rd = request.getRequestDispatcher("search.html");
-              rd.forward(request, response);
-          }  
-          else {
-              response.sendRedirect("invalid.html");
-              
-               //    forward thường sẽ để các servlet giao tiếp với nhau, actión phía servers. Thường thì sẽ liên quan đến việc cho server tiếp tục xử lí nhiều công đoạn khác nhau.
-               //    redirect thì sẽ dành cho phía client. Đơn giản vì họ không thể can thiệp về dữ liệu ở server. Việc của họ là sử dụng ui và chức năng phía FE.      
-              
-          }
-        }
-        catch (Exception e){
-            out.print.("Error: " + e.getMessage());
-        }
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Servlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
 
+            String id = request.getParameter("id");
+            String fullname = request.getParameter("fullname");
+            String gender = request.getParameter("gender");
+            
+            out.println("<h1>Infomation</h1><br/>");
+            out.println("<p>ID: " + id + " </p><br/>");
+            out.println("<p>Fullname: " + fullname + " </p></br>");
+            out.println("<p>Gender: " + gender + " </p><br/>");
+                    
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
